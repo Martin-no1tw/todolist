a95f63b2bed1c4a551389a13431bc55cbf3974c2
Martin-no1tw <kljhger7728@gmail.com>
2021-10-13 22:44:53 +0800

    feat: add handlebars

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 // 載入 express 並建構應用程式伺服器
 const express = require('express')
 const app = express()
+const exphbs = require('express-handlebars')
+
+app.engine('hbs', exphbs({ defaultlayout: 'main', extname: '.hbs' }))
+app.set('view engine', 'hbs')
+
 
 // 設定首頁路由
 app.get('/', (req, res) => {
-  res.send('hello world')
+  res.render('index')
 })
 
 // 設定 port 3000
