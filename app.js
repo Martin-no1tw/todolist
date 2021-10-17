58ce7a1ce5d1f4a1be45ded04f89264af3af9522
Martin-no1tw <kljhger7728@gmail.com>
2021-10-17 22:31:53 +0800

    chore: heroku init

--- app.js
+++ app.js
@@ -10,7 +10,11 @@ const PORT = process.env.PORT || 3000
 const Todo = require('./models/todo')
 
 const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/todolist'
-mongoose.connect(MONGODB_URI)
+
+mongoose.connect(MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
 
 const db = mongoose.connection
 
