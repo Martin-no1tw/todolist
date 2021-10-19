716e3586a9f4c680e8c1c88cc4ce7195838448f2
Martin-no1tw <kljhger7728@gmail.com>
2021-10-19 22:24:47 +0800

    feat: router

--- models/Routes/modules/todos.js
+++ models/Routes/modules/todos.js
@@ -2,43 +2,36 @@ const express = require('express')
 const router = express.Router()
 const Todo = require('../../models/todo')
 // 設定首頁路由
-router.get('/', (req, res) => {
-  Todo.find() // 取出 Todo model 裡的所有資料
-    .lean() // 把 Mongoose 的 Model 物件轉換成乾淨的 JavaScript 資料陣列
-    .then(todos => res.render('index', { todos })) // 將資料傳給 index 樣板
-    .catch(error => console.error(error)) // 錯誤處理
-})
 
-router.get('/todos/new', (req, res) => {
+router.get('/new', (req, res) => {
   return res.render('new')
 })
-
 router.use(bodyParser.urlencoded({ extended: true }))
 
-router.post('/todos', (req, res) => {
-  const name = req.body.name       // 從 req.body 拿出表單裡的 name 資料
-  return Todo.create({ name })     // 存入資料庫
-    .then(() => res.redirect('/')) // 新增完成後導回首頁
+router.post('/', (req, res) => {
+  const name = req.body.name
+  return Todo.create({ name })
+    .then(() => res.redirect('/'))
     .catch(error => console.log(error))
 })
 
-router.get('/todos/:id', (req, res) => {
+router.get('/:id', (req, res) => {
   const id = req.params.id
   return Todo.findById(id)
     .lean()
-    .then((todo) => res.render('detail', { todo }))
+    .then(todo => res.render('detail', { todo }))
     .catch(error => console.log(error))
 })
 
-router.get('/todos/:id/edit', (req, res) => {
+router.get('/:id/edit', (req, res) => {
   const id = req.params.id
   return Todo.findById(id)
     .lean()
-    .then((todo) => res.render('edit', { todo }))
+    .then(todo => res.render('edit', { todo }))
     .catch(error => console.log(error))
 })
 
-router.post('/todos/:id/edit', (req, res) => {
+router.put('/:id', (req, res) => {
   const id = req.params.id
   const { name, isDone } = req.body
   return Todo.findById(id)
@@ -51,11 +44,11 @@ router.post('/todos/:id/edit', (req, res) => {
     .catch(error => console.log(error))
 })
 
-router.post('/todos/:id/delete', (req, res) => {
+router.delete('/:id', (req, res) => {
   const id = req.params.id
-    return Todo.findById(id)
+  return Todo.findById(id)
     .then(todo => todo.remove())
     .then(() => res.redirect('/'))
     .catch(error => console.log(error))
-}) 
-  module.exports =  router  
+})
+.exports = router
